6dd198a93da38f6956b2d8214fceeafb8a645ebf
like <747367352@qq.com>
2020-08-08 18:49:22 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 
-for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "8091" ^| findstr "LISTENING"') do set pid=%%a
+for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "3000" ^| findstr "LISTENING"') do set pid=%%a
 if not "%pid%" == "" (
   taskkill /f /PID %pid%
 ) else (

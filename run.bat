b4cb3b0bc1272fb3ddebe3896a60c3a997cd8c07
like <747367352@qq.com>
2020-08-08 20:16:54 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,7 @@
-
 for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "3000" ^| findstr "LISTENING"') do set pid=%%a
 if not "%pid%" == "" (
   taskkill /f /PID %pid%
 ) else (
   rem echo Server is not running.
 )
-npm start
-pause
+start /b npm start

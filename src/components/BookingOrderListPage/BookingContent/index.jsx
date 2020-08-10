9b2ddd7f8d1e8f1d04d59e95b70a039d38460272
fox <1036674617@qq.com>
2020-08-11 00:31:36 +0800

    [York] feature: add BookingOrderList page ui

--- src/components/BookingOrderListPage/BookingContent/index.jsx
+++ src/components/BookingOrderListPage/BookingContent/index.jsx
@@ -14,7 +14,39 @@ class BookingContent extends React.Component {
         return (
           <div className="booking-content-wrapper">
             <div className="top-search"><span>输入手机号查询</span></div>
-            <div className="booking-content" />
+            <div className="booking-content-center">
+              <div className="booking-content">
+                <div className="content-title">
+                  订单详情
+                </div>
+                <div className="content-table">
+                  <table border="1" bordercolor="#cc0000">
+                    <tbody>
+                    <tr>
+                      <th>停车场</th>
+                      <td>xxx停车场</td>
+                      <th>位置</th>
+                      <td style={{width: "200px"}}>香洲区xxx路xx</td>
+                      <th>车牌号</th>
+                      <td style={{width: "180px"}}>粤C88888</td>
+                    </tr>
+                    <tr>
+                      <th>时间段</th>
+                      <td>2020/08/09 15:30 - 2020/08/09 16:00</td>
+                      <th>手机</th>
+                      <td style={{width: "200px"}}>13811111111</td>
+                      <th>邮箱</th>
+                      <td style={{width: "180px"}}>88888888@qq.com</td>
+                    </tr>
+                    <tr>
+                      <th>费用</th>
+                      <td>¥12</td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
             <div className="show-logo" />
           </div>
 )

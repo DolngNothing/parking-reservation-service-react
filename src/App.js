8a8e4b0dbedcf774255fa7183e44a7216b6c06a1
DolngNothing <1178363215@qq.com>
2020-08-10 13:57:32 +0800

    [christopher] feature: add OrderCreatePageContaine

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import IndexContainer from './containers/IndexContainer';
 import OrderDetail from './views/OrderDetail'
 import { HashRouter, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import OrderCreatePageContainer from './containers/OrderCreatePageContainer'
+
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <Route path="/" exact component={IndexContainer} />
         <Route path="/orderDetail" component={OrderDetail} />
+        <Route path="/OrderCreatePageContainer" component={OrderCreatePageContainer} />
       </HashRouter>
     </div>
   );

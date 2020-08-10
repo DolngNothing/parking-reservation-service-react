97ccadf56c3812d40de722209810d449f36c2c58
hzhgit <971499086@qq.com>
2020-08-10 15:00:35 +0800

    [zach]feature:implement click comfirm button change order status and hide the comfirm button

--- src/views/OrderDetail/index.js
+++ src/views/OrderDetail/index.js
@@ -1,18 +1,45 @@
 import React from 'react'
-import { Descriptions, Button, Alert } from 'antd'
+import { Descriptions, Button, notification } from 'antd'
 import './index.scss'
 
 class OrderDetail extends React.Component {
-    state = {
-        size: 'default',
-    };
+    constructor(props) {
+        super(props)
+        this.state = {
+            order: {
+                parkingLotName: "XXX停车场",
+                postion: "香洲区XXX路XX",
+                carNumber: "粤C7777",
+                timeSection: "2020年8月9日 15:30 - 2020年8月9日 16:00",
+                phone: "13631230000",
+                mail: "971499022@qq.com",
+                cost: 11,
+                status: false,
+            }
+        };
+    }
 
-    onChange = e => {
-        console.log('size checked', e.target.value);
-        this.setState({
-            size: e.target.value,
+
+    comfirmOrder = () => {
+        notification['success']({
+            message: 'Notification Title',
+            description:
+                '预约成功！',
         });
-    };
+        this.setState({
+            order: {
+                status: true
+            }
+        })
+    }
+
+    cancelOrder = () => {
+        let date = new Date();
+        console.log(date);
+    }
+
+    componentDidMount() {
+    }
 
     render() {
         return (
@@ -22,27 +49,32 @@ class OrderDetail extends React.Component {
                     title="订单详情"
                     size="default"
                 >
-                    <Descriptions.Item label="停车场">XXX停车场</Descriptions.Item>
-                    <Descriptions.Item label="位置">香洲区XXX路XX</Descriptions.Item>
-                    <Descriptions.Item label="车牌号">粤C666</Descriptions.Item>
-                    <Descriptions.Item label="时间段">2020年8月9日 15:30 - 2020年8月9日 16:00</Descriptions.Item>
-                    <Descriptions.Item label="手机">13631230001</Descriptions.Item>
-                    <Descriptions.Item label="邮箱">971499022@qq.com</Descriptions.Item>
-                    <Descriptions.Item label="费用">12￥</Descriptions.Item>
-                    <Descriptions.Item label="订单状态">等待确认/预约成功</Descriptions.Item>
+                    <Descriptions.Item label="停车场">{this.state.order.parkingLotName}</Descriptions.Item>
+                    <Descriptions.Item label="位置">{this.state.order.postion}</Descriptions.Item>
+                    <Descriptions.Item label="车牌号">{this.state.order.carNumber}</Descriptions.Item>
+                    <Descriptions.Item label="时间段">{this.state.order.timeSection}</Descriptions.Item>
+                    <Descriptions.Item label="手机">{this.state.order.phone}</Descriptions.Item>
+                    <Descriptions.Item label="邮箱">{this.state.order.mail}</Descriptions.Item>
+                    <Descriptions.Item label="费用">{this.state.order.cost}￥</Descriptions.Item>
+                    <Descriptions.Item label="订单状态">
+                        <span className={this.state.order.status ? "completed" : "uncompleted"}>
+                            {this.state.order.status ? "预约成功" : "取消预约"}
+                        </span>
+                    </Descriptions.Item>
                 </Descriptions>
 
                 <div className="btn-div">
-                    <Button type="primary">确认订单</Button>
-                    <Button type="">取消预约</Button>
+                    <div onClick={this.comfirmOrder} className={this.state.order.status ? "comfirm" : "uncomfirm"}>
+                        <Button type="primary">
+                            确认订单
+                        </Button>
+                    </div>
+                    <div>
+                        <Button onClick={this.cancelOrder}>取消预约</Button>
+                    </div>
                 </div>
 
-                <Alert
-                    message="Success Tips"
-                    description="Detailed description and advice about successful copywriting."
-                    type="success"
-                    showIcon
-                />
+
             </div>
         );
     }

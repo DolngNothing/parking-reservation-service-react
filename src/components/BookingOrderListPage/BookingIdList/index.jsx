215fb4e8f5f8e82b4109906ac6d83820a1dd67f8
ZHANGYO5 <york.zhang@oocl.com>
2020-08-10 15:28:25 +0800

    [York] feature: complete BookingIdList ui

--- src/components/BookingOrderListPage/BookingIdList/index.jsx
+++ src/components/BookingOrderListPage/BookingIdList/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import './index.scss'
+import '../../../css/icon.css'
 
 class BookingIdList extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            idList: [
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+                "xxxxxxxxxxxxx",
+            ]
+        }
     }
 
     render() {
         return <div className="booking-id-list">
-            
-        </div>
+                <div className="title-text">订单编号</div>
+                {this.state.idList.map((item, index) => <div className="id-text" key={index}><span className="icon-booking"></span>{item}</div>)}
+            </div>
     }
 }
 

8957f362cec592f4fcf51c8bc58a6cb7e7076b79
ZHANGYO5 <york.zhang@oocl.com>
2020-08-10 11:08:28 +0800

    [York] feature: add bookingOrderList page

---
+++ src/views/bookingOrderList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class BookingOrderList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div className="booking-list-page">
+
+        </div>
+    }
+}
+
+export default BookingOrderList

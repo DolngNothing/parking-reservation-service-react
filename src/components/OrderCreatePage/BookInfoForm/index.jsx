4612ded524f1c5f2b2f33afd6f0b07ca146e4770
DolngNothing <1178363215@qq.com>
2020-08-10 20:52:18 +0800

    [001][christopher] feature: implement bookInfoForm

--- src/components/OrderCreatePage/BookInfoForm/index.jsx
+++ src/components/OrderCreatePage/BookInfoForm/index.jsx
@@ -9,115 +9,144 @@ const phoneValit = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])
 const mailValit = /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 class BookInfoForm extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            phoneValitType: 0,
-            liscenValiType: 0,
-            mailValiType: 0,
-            liscen: '',
-            phone: '',
-            mail: '',
-            startTime: '',
-            endTime: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneValitType: 0,
+      liscenValiType: 0,
+      mailValiType: 0,
+      liscen: '',
+      phone: '',
+      mail: '',
+      startTime: '',
+      endTime: ''
     }
+  }
 
-    isLicenseNo = (e) => {
-        if (licenseVailt.test(e.target.value)) {
-            this.setState({ liscenValiType: 1 })
-            this.setState({ liscen: e.target.value })
-        } else {
-            this.setState({ liscenValiType: 2 })
-            this.setState({ liscen: '' })
-        }
+  isLicenseNo = (e) => {
+    if (licenseVailt.test(e.target.value)) {
+      this.setState({ liscenValiType: 1 })
+      this.setState({ liscen: e.target.value })
+    } else {
+      this.setState({ liscenValiType: 2 })
+      this.setState({ liscen: '' })
     }
+  }
 
-    isPhoneNo = (e) => {
-        if (phoneValit.test(e.target.value)) {
-            this.setState({ phoneValitType: 1 })
-            this.setState({ phone: e.target.value })
-        } else {
-            this.setState({ phoneValitType: 2 })
-            this.setState({ phone: '' })
-        }
+  isPhoneNo = (e) => {
+    if (phoneValit.test(e.target.value)) {
+      this.setState({ phoneValitType: 1 })
+      this.setState({ phone: e.target.value })
+    } else {
+      this.setState({ phoneValitType: 2 })
+      this.setState({ phone: '' })
     }
+  }
 
-    isEmailNo = (e) => {
-        if (mailValit.test(e.target.value)) {
-            this.setState({ mailValiType: 1 })
-            this.setState({ mail: e.target.value })
-        } else {
-            this.setState({ mailValiType: 2 })
-            this.setState({ mail: '' })
-        }
+  isEmailNo = (e) => {
+    if (mailValit.test(e.target.value)) {
+      this.setState({ mailValiType: 1 })
+      this.setState({ mail: e.target.value })
+    } else {
+      this.setState({ mailValiType: 2 })
+      this.setState({ mail: '' })
     }
+  }
 
-    setDate = (e) => {
-        this.setState({ startTime: e[0]._d })
-        this.setState({ endTime: e[1]._d })
+  setDate = (e) => {
+    if(e!=null){
+    this.setState({ startTime: e[0]._d })
+    this.setState({ endTime: e[1]._d })
+    }else{
+      this.setState({ startTime: '' })
+      this.setState({ endTime: '' })
     }
+  }
 
+  saveBookForm = () => {
+    const {liscen,phone,mail,startTime,endTime} =this.state;
     
-
-    render() {
-      const {liscenValiType}=this.state;
-      const {phoneValitType}=this.state;
-      const {mailValiType}=this.state;
-        return (
-          <div>
-            <Form>
-              <Form.Item
-                help="请填写正确的车牌号"
-                validateStatus={valitype[liscenValiType]}
-                hasFeedback
-                rules={[{ required: true }]}
-              >
-                <Input
-                  prefix={<CarOutlined />}
-                  onBlur={this.isLicenseNo}
-                />
-              </Form.Item>
-              <Form.Item
-                help="请填写正确的手机号"
-                validateStatus={valitype[phoneValitType]}
-                hasFeedback
-              >
-                <Input
-                  rules={[{ required: true }]}
-                  prefix={<PhoneOutlined />}
-                  onBlur={this.isPhoneNo}
-                />
-              </Form.Item>
-              <Form.Item
-                help="请填写正确的邮箱号码"
-                validateStatus={valitype[mailValiType]}
-                hasFeedback
-              >
-                <Input
-                  rules={[{ required: true }]}
-                  prefix={<MailOutlined />}
-                  onBlur={this.isEmailNo}
-                />
-              </Form.Item>
-              <Form.Item
-                rules={[{ required: true }]}
-                hasFeedback
-              >
-                <RangePicker showTime onChange={this.setDate} />
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" onClick>
-                  Check
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        );
+    if(liscen!=''&&phone!=''&&mail!=''&&startTime!=''&&endTime!=''){
+      console.log('可以传输啦！');
+    }else{
+      alert("请输入时间")
     }
+
+
+  //   this.axios.post(URL,{'liscen':liscen,'phone':phone,
+  //   'mail':mail,'startTime':startTime,'endTime':endTime
+  // })
+
+  }
+
+  render() {
+    const formItemLayout ={
+      labelCol: { span: 4 },
+      wrapperCol: { span: 6 },
+    }
+    const buttonItemLayout ={
+      wrapperCol: { span: 6, offset: 0 },
+    }
+    const { liscenValiType } = this.state;
+    const { phoneValitType } = this.state;
+    const { mailValiType } = this.state;
+    return (
+      <div>
+        <Form {...formItemLayout}
+        layout={'horizontal'}
+        >
+          <Form.Item
+            help="请填写正确的车牌号"
+            validateStatus={valitype[liscenValiType]}
+            hasFeedback
+            rules={[{ required: true }]}
+          >
+            <Input
+              prefix={<CarOutlined />}
+              onBlur={this.isLicenseNo}
+              required
+            />
+          </Form.Item>
+          <Form.Item
+            help="请填写正确的手机号"
+            validateStatus={valitype[phoneValitType]}
+            hasFeedback
+          >
+            <Input
+              rules={[{ required: true }]}
+              prefix={<PhoneOutlined />}
+              onBlur={this.isPhoneNo}
+              required
+            />
+          </Form.Item>
+          <Form.Item
+            help="请填写正确的邮箱号码"
+            validateStatus={valitype[mailValiType]}
+            hasFeedback
+          >
+            <Input
+              rules={[{ required: true }]}
+              prefix={<MailOutlined />}
+              onBlur={this.isEmailNo}
+              required
+            />
+          </Form.Item>
+          <Form.Item
+            rules={[{ required: true, message: 'Please input your password!' }]}
+            hasFeedback
+            required
+          >
+            <RangePicker showTime onChange={this.setDate}  />
+          </Form.Item>
+          <Form.Item {...buttonItemLayout}>
+            <Button type="primary" htmlType="submit" onClick={this.saveBookForm}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
 }
-
-
-
 
 export default BookInfoForm;
--- src/views/OrderCreatePage/index.jsx
+++ src/views/OrderCreatePage/index.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ParkingLotInfo from '../../components/OrderCreatePage/ParkingLotInfo'
 import BookInfoForm from '../../components/OrderCreatePage/BookInfoForm'
+import { Row, Col } from 'antd';
+import './index.css'
 
-class OrderCreatePage extends React.Component{
+class OrderCreatePage extends React.Component {
 
-    render(){
-        return (
-          <div>
-            <ParkingLotInfo />
-            <BookInfoForm />
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col span={8}><ParkingLotInfo /></Col>
+          <Col span={16}><BookInfoForm /></Col>
+        </Row>
+        
+        
+      </div>
+    );
+  }
 }
 
 export default OrderCreatePage;

2e900e633f0a10eb386b94050e3d982293436917
hzhgit <971499086@qq.com>
2020-08-13 18:52:19 +0800

    [zach]feature:add back to home at book list

--- src/views/bookingOrderList/index.jsx
+++ src/views/bookingOrderList/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PageHeader } from 'antd';
 import { LeftOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom'
 import BookingIdList from '../../components/BookingOrderListPage/BookingIdList'
 import BookingContent from '../../components/BookingOrderListPage/BookingContent'
 import './index.scss'
@@ -11,16 +12,18 @@ class BookingOrderList extends React.Component {
     return (
       <div className="booking-list-page">
         <PageHeader>
-          <span className="span-back">
-<LeftOutlined />
-返回首页
+          <Link to="/">
+            <span className="span-back">
+              <LeftOutlined />
+            返回首页
           </span>
+          </Link>
           &nbsp;&nbsp;|
           <span className="span-title">订单详情</span>
         </PageHeader>
         <BookingIdList
-setBookOrder={this.props.setBookOrder}
-                      bookOrder={this.props.bookOrder}
+          setBookOrder={this.props.setBookOrder}
+          bookOrder={this.props.bookOrder}
         />
         <BookingContent
           bookOrder={this.props.bookOrder}

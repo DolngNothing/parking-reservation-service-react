4caa5615ea5a672677df21c7ae7fba0dec0af7e1
hzhgit <971499086@qq.com>
2020-08-13 20:40:23 +0800

    [zach]feature:add nav bar at order create page

--- src/views/OrderCreatePage/index.jsx
+++ src/views/OrderCreatePage/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { PageHeader } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom'
 import ParkingLotInfo from '../../components/OrderCreatePage/ParkingLotInfo'
 import BookInfoForm from '../../components/OrderCreatePage/BookInfoForm'
 import './index.css'
@@ -8,26 +11,35 @@ class OrderCreatePage extends React.Component {
 
   render() {
     return (
-
-      <div className="container">
-        <div className="content">
-          <div className="item">
-            <ParkingLotInfo
-              parkingLot={this.props.parkingLot}
-              emptyPosition={this.props.emptyPosition}
-            />
-          </div>
-          <div className="item">
-            <BookInfoForm
-              parkingLot={this.props.parkingLot}
-              setBookOrder={this.props.setBookOrder}
-              history={this.props.history}
-              saveEmptyPosition={this.props.saveEmptyPosition}
-            />
+      <div>
+        <PageHeader>
+          <Link to="/">
+            <span className="span-back">
+              <LeftOutlined />
+                  返回首页
+            </span>
+          </Link>
+      &nbsp;&nbsp;|
+        </PageHeader>
+        <div className="container">
+          <div className="content">
+            <div className="item">
+              <ParkingLotInfo
+                parkingLot={this.props.parkingLot}
+                emptyPosition={this.props.emptyPosition}
+              />
+            </div>
+            <div className="item">
+              <BookInfoForm
+                parkingLot={this.props.parkingLot}
+                setBookOrder={this.props.setBookOrder}
+                history={this.props.history}
+                saveEmptyPosition={this.props.saveEmptyPosition}
+              />
+            </div>
           </div>
         </div>
       </div>
-
     );
   }
 }
